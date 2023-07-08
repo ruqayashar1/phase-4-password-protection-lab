ace5012a589e46cee6521ac0190a8c500179c63e
rukia <ruqayashar1@gmail.com>
2023-07-08 15:14:22 +0300

    completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  post '/login', to: 'sessions#create'
+  get '/me', to: 'users#show'
+  delete '/logout', to: 'sessions#destroy'
+  post '/signup', to: 'users#create'
+  delete '/logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
